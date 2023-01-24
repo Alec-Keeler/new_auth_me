fb289c433dac3febd31e2bcea7eeebfdf4268df8
Alec Keeler <alec.keeler@gmail.com>
2023-01-24 09:09:11 -0600

    boardgames route

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js');
+const gamesRouter = require('./games.js');
 const { restoreUser } = require('../../utils/auth.js');
 
 router.use(restoreUser);
@@ -9,6 +10,8 @@ router.use(restoreUser);
 router.use('/session', sessionRouter);
 
 router.use('/users', usersRouter);
+
+router.use('/boardgames', gamesRouter)
 
 // router.post('/test', function (req, res) {
 //     res.json({ requestBody: req.body });
